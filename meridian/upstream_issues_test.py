2f5d0bec0092785a83f958dee6da935a0c0d13e7
Sundar Ramesh Kumar <euross4@gmail.com>
2026-09-15 17:14:03 +0530

    Set the fork's copyright holder
    Replace the "Meridian fork contributors" placeholder with the repository
    owner, taken from the git identity on every commit in this branch. Change
    it if the fork should be owned by a legal entity instead of an individual.

--- meridian/upstream_issues_test.py
+++ meridian/upstream_issues_test.py
@@ -1,4 +1,4 @@
-# Copyright 2026 Meridian fork contributors.
+# Copyright 2026 Sundar Ramesh Kumar.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
